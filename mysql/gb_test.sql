b8ca84f11490c3d24a124df5c0d2d2345f5966b3
Massimo Siboldi <mdsiboldi@gmail.com>
2015-04-07 21:42:12 -0700

    testing automatic mysql update with commit once more

--- mysql/gb_test.sql
+++ mysql/gb_test.sql
@@ -25,7 +25,8 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `email` varchar(254) DEFAULT NULL,
   `password` varchar(256) DEFAULT NULL,
-  `username` varchar(36) DEFAULT NULL
+  `username` varchar(36) DEFAULT NULL,
+  `type` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -47,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-07 21:40:55
+-- Dump completed on 2015-04-07 21:42:12
