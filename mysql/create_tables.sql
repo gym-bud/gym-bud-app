22755034bf45d195702ac95e4d73663c07dd07f1
Eric Buckthal <ebuckthal@gmail.com>
2015-05-13 22:45:59 -0700

    mysql modifications

--- mysql/create_tables.sql
+++ mysql/create_tables.sql
@@ -1,29 +1,35 @@
-DROP TABLE IF EXISTS `gym_employee`;
-DROP TABLE IF EXISTS `gym`;
-DROP TABLE IF EXISTS `gym_group_admin`;
-DROP TABLE IF EXISTS `gym_group`;
-DROP TABLE IF EXISTS `user`;
-
 CREATE TABLE `user` (
    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
    `email` VARCHAR(50) NOT NULL UNIQUE,
    `password` VARCHAR(60) NOT NULL,
    `first_name` VARCHAR(50) NOT NULL,
    `last_name` VARCHAR(50) NOT NULL,
-   `create_date` DATE NOT NULL,
-   `modify_date` DATE NOT NULL
+   `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+   `updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP 
 ) 
 ENGINE = InnoDB; 
 
+CREATE TABLE `system_admin` (
+   `user_id` INT UNSIGNED NOT NULL,
+   FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+   `create_date` DATETIME NOT NULL,
+   `modify_date` DATETIME NOT NULL
+)
+ENGINE = InnoDB;
+
 CREATE TABLE `gym_group` (
    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   `name` VARCHAR(100) NOT NULL UNIQUE
+   `name` VARCHAR(100) NOT NULL UNIQUE,
+   `create_date` DATE NOT NULL,
+   `modify_date` DATE NOT NULL
 ) 
 ENGINE = InnoDB;
 
 CREATE TABLE `gym_group_admin` (
    `gym_group_id` INT UNSIGNED NOT NULL,
    `user_id` INT UNSIGNED NOT NULL, 
+   `create_date` DATE NOT NULL,
+   `modify_date` DATE NOT NULL,
    FOREIGN KEY (gym_group_id) REFERENCES gym_group(id) ON DELETE CASCADE,
    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 ) 
@@ -33,6 +39,8 @@ CREATE TABLE `gym` (
    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
    `gym_group_id` INT UNSIGNED NOT NULL,
    `name` VARCHAR(100) NOT NULL UNIQUE,
+   `create_date` DATE NOT NULL,
+   `modify_date` DATE NOT NULL,
    FOREIGN KEY (gym_group_id) REFERENCES gym_group(id) ON DELETE CASCADE
 ) 
 ENGINE = InnoDB;
@@ -40,6 +48,8 @@ ENGINE = InnoDB;
 CREATE TABLE `gym_employee` (
    `gym_id` INT UNSIGNED NOT NULL,
    `user_id` INT UNSIGNED NOT NULL,
+   `create_date` DATE NOT NULL,
+   `modify_date` DATE NOT NULL,
    FOREIGN KEY (gym_id) REFERENCES gym(id) ON DELETE CASCADE,
    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 ) 
---
+++ mysql/drop_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS `gym_employee`;
+DROP TABLE IF EXISTS `gym`;
+DROP TABLE IF EXISTS `gym_group_admin`;
+DROP TABLE IF EXISTS `gym_group`;
+DROP TABLE IF EXISTS `system_admin`;
+DROP TABLE IF EXISTS `user`;
